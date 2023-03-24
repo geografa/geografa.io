665e3c14b9085265f49a87131bd3579aeaed13cb
Rafa <rafa@caras-MBP.Home>
2023-03-23 19:22:18 -0700

    first commit

---
+++ tmp.js
@@ -0,0 +1,68 @@
+{
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-117.881072, 33.6714212]
+      },
+      properties: {
+        name: "Kristin G. 2",
+        phoneFormatted: "(111) 111-1111",
+        phone: "1111111111",
+        address: "151 Kalmus Dr M-1",
+        city: "Costa Mesa",
+        country: "United States",
+        crossStreet: "at Ref Hill Ave.",
+        postalCode: "92626",
+        state: "CA",
+        instagram: "@testinstagram",
+        imagesrc:
+          "https://cdn.shopify.com/s/files/1/0014/7596/9087/files/kristin_grip_round.png?v=1678266700"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-77.034084142948, 38.909671288923]
+      },
+      properties: {
+        name: "test-name",
+        phoneFormatted: "(202) 234-7336",
+        phone: "2022347336",
+        address: "1471 P St NW",
+        city: "Washington DC",
+        country: "United States",
+        crossStreet: "at 15th St NW",
+        postalCode: "20005",
+        state: "D.C.",
+        instagram: "@testinstagram",
+        imagesrc:
+          "https://cdn.shopify.com/s/files/1/0014/7596/9087/files/kristin_grip_round.png?v=1678266700"
+      }
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [-117.881072, 33.6714112]
+      },
+      properties: {
+        name: "Kristin G.",
+        phoneFormatted: "(111) 111-1111",
+        phone: "1111111111",
+        address: "151 Kalmus Dr M-1",
+        city: "Costa Mesa",
+        country: "United States",
+        crossStreet: "at Ref Hill Ave.",
+        postalCode: "92626",
+        state: "CA",
+        instagram: "@testinstagram",
+        imagesrc:
+          "https://cdn.shopify.com/s/files/1/0014/7596/9087/files/kristin_grip_test.jpg?v=1678053224"
+      }
+    }
+  ]
+}
